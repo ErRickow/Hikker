5e52fb19c10bd6dcc258b2a33f81bcf8cd967b01
Just R <remixingmagelang@gmail.com>
2024-08-16 06:47:22 +0700

    update hikka/inline/token_obtainment.py

--- hikka/inline/token_obtainment.py
+++ hikka/inline/token_obtainment.py
@@ -149,7 +149,7 @@ class TokenObtainment(InlineUnit):
                         "hikka.inline",
                         "custom_bot",
                         False,
-                    ) and not re.search(r"@hikka_[0-9a-zA-Z]{6}_bot", button.text):
+                    ) and not re.search(r"@erhikbot_[0-9a-zA-Z]{6}_bot", button.text):
                         continue
 
                     await fw_protect()
